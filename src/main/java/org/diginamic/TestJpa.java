e011d5cf7715a27b6eec57cf784708262f43398a
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-30 12:13:26 +0200

    Recherche livre en fonction de son titre via Query

--- src/main/java/org/diginamic/TestJpa.java
+++ src/main/java/org/diginamic/TestJpa.java
@@ -3,6 +3,7 @@ package org.diginamic;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +18,19 @@ public class TestJpa {
 		EntityManagerFactory entityManFactory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager entityMan = entityManFactory.createEntityManager();
 		
-		Livre l = entityMan.find(Livre.class, 1);
+		Livre livre = entityMan.find(Livre.class, 1);
 		
-		LOG.debug("ID : " + l.getId());
-		LOG.debug("Titre : " + l.getTitre());
-		LOG.debug("Auteur : " + l.getAuteur());
+		LOG.debug("ID : " + livre.getId());
+		LOG.debug("Titre : " + livre.getTitre());
+		LOG.debug("Auteur : " + livre.getAuteur());
+		
+		Query query = entityMan.createQuery("SELECT l FROM Livre l WHERE l.titre = :titreRecherche");
+		query.setParameter("titreRecherche", "Germinal");
+		Livre livreSelect = (Livre) query.getResultList().get(0);
+		
+		LOG.debug("ID : " + livreSelect.getId());
+		LOG.debug("Titre : " + livreSelect.getTitre());
+		LOG.debug("Auteur : " + livreSelect.getAuteur());
 	}
 
 }
