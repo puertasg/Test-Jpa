6230fd93528afba539b64c47d1c43463d514b2d6
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-30 11:12:12 +0200

    Implémentation logger, instanciation livre à partir d'un EntityManager, renseignement champ titre, ajout getter pour id

--- src/main/java/Entities/Livre.java
+++ src/main/java/Entities/Livre.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -10,8 +11,15 @@ public class Livre {
 	@Id
 	private int id;
 	
+	@Column(name="TITRE", length = 255, nullable = false)
+	private String titre;
+	
 	public Livre()
 	{
 		
+	}
+
+	public int getId() {
+		return id;
 	}
 }
--- src/main/java/org/diginamic/TestJpa.java
+++ src/main/java/org/diginamic/TestJpa.java
@@ -4,11 +4,22 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import Entities.Livre;
+
 public class TestJpa {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TestJpa.class);
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManFactory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager entityMan = entityManFactory.createEntityManager();
+		
+		Livre l = entityMan.find(Livre.class, 1);
+		
+		LOG.debug("ID : " + l.getId());
 	}
 
 }
