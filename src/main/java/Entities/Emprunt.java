dcaec4fb3b59a194d8235e1dfd06361714462c45
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-30 12:22:11 +0200

    Ajout entité Emprunt, mappage des champs

---
+++ src/main/java/Entities/Emprunt.java
@@ -0,0 +1,33 @@
+package Entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="livre")
+public class Emprunt {
+	
+	@Id
+	private int id;
+	
+	@Column(name="DATE_DEBUT", nullable=false)
+	private LocalDate dateDebut;
+	
+	@Column(name="DATE_FIN", nullable=true)
+	private LocalDate dateFin;
+	
+	@Column(name="DELAI", length = 10, nullable=true)
+	private int delai;
+	
+	@Column(name="ID_CLIENT", length = 10, nullable=false)
+	private int idClient;
+	
+	public Emprunt()
+	{
+		
+	}
+}
