9ccf56a99ffd3160e57067952367ec914cc2d1b0
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-30 17:27:26 +0200

    Implémentation entité Compte

--- src/main/java/banque/entities/Compte.java
+++ src/main/java/banque/entities/Compte.java
@@ -1,5 +1,25 @@
 package banque.entities;
 
-public class Compte {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="compte")
+public class Compte {
+	
+	@Id
+	private int idCompte;
+	
+	@Column(name="NUMERO_COMPTE", length = 40, nullable=false)
+	private String numeroCompte;
+	
+	@Column(name="SOLDE_COMPTE", length = 40, nullable=false)
+	private double soldeCompte;
+	
+	public Compte()
+	{
+		
+	}
 }
