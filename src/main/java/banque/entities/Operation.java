5e15928df14a26a52464d97621431b289993bc0c
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-30 17:32:52 +0200

    Implémentation entité Operation

--- src/main/java/banque/entities/Operation.java
+++ src/main/java/banque/entities/Operation.java
@@ -1,5 +1,30 @@
 package banque.entities;
 
-public class Operation {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="operation")
+public class Operation {
+	
+	@Id
+	private int idOperation;
+	
+	@Column(name="DATE_OPERATION", nullable=false)
+	private LocalDate dateOperation;
+	
+	@Column(name="MONTANT", length=40, nullable=false)
+	private double montant;
+	
+	@Column(name="MOTIF", length=40, nullable=false)
+	private String motif;
+	
+	public Operation()
+	{
+		
+	}
 }
